9028a271289b0f95837916f4deb8370f4fe8ab7c
Sam White <samuelwhite98@outlook.com>
2022-11-19 10:13:09 +0000

    Add HTML/CSS for tabs,add js to check selected tab

--- script.js
+++ script.js
@@ -3,11 +3,25 @@ function showItems(parent){
     dropdown.classList.toggle("display")
 }
 
-const navButtons = (() => {
+function chooseTab(selectedTab){
+    if (selectedTab.querySelector(".tab-selected") === null){
+        console.log("not selected")
+    }
+}
+
+const navController = (() => {
     let dropdowns = Array.from(document.querySelectorAll(".dropbtn"))
     dropdowns.forEach(dropdown => dropdown.addEventListener("click", (e) => {
         let parent = e.currentTarget
         showItems(parent)
+    }))
+})();
+
+const tabController = (() => {
+    let tabButtons = Array.from(document.querySelectorAll(".tab"))
+    tabButtons.forEach(tab => tab.addEventListener("click", (e) => {
+        let selectedTab = e.currentTarget
+        chooseTab(selectedTab)
     }))
 })();
 
