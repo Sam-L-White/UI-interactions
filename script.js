8a1f0446ba0e35b8f16ac600106a76f76e1a42c4
Sam White <samuelwhite98@outlook.com>
2023-03-09 21:46:42 +0000

    Add ui interactions

--- script.js
+++ script.js
@@ -13,6 +13,39 @@ function chooseTab(selectedTab){
     }
 }
 
+function moveImage(direction){
+    let currentImage = document.querySelector(".img-show");
+    let allImages = document.querySelectorAll(".image");
+    let currentIndex
+    let imagesArray = []
+    let x = 0
+    allImages.forEach(image => {
+        imagesArray[x] = image
+        if(image == currentImage){
+            currentIndex = x;
+        }
+        x++
+    })
+    if(direction == 'forward'){
+        if(currentIndex == (imagesArray.length - 1)){
+            imagesArray[currentIndex].classList.remove('img-show');
+            imagesArray[0].classList.add('img-show');
+        } else {
+            imagesArray[currentIndex].classList.remove('img-show');
+            imagesArray[currentIndex].nextElementSibling.classList.add('img-show');
+        }
+        
+    } else if (direction == 'back'){
+        if(currentIndex === 0){
+            imagesArray[currentIndex].classList.remove('img-show');
+            imagesArray[imagesArray.length - 1].classList.add('img-show');
+        } else {
+            imagesArray[currentIndex].classList.remove('img-show');
+            imagesArray[currentIndex].previousElementSibling.classList.add('img-show');
+        }
+    }
+}
+
 const navController = (() => {
     let dropdowns = Array.from(document.querySelectorAll(".dropbtn"))
     dropdowns.forEach(dropdown => dropdown.addEventListener("click", (e) => {
@@ -29,5 +62,17 @@ const tabController = (() => {
     }))
 })();
 
+const imageController = (() => {
+    let backButton = document.querySelector(".back");
+    let forwardButton = document.querySelector(".forward");
+
+    backButton.addEventListener("click", (e) => {
+        moveImage('back')
+    })
+
+    forwardButton.addEventListener("click", (e) => {
+        moveImage('forward')
+    })
+})();
 
 
